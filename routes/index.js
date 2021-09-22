9526798fc6d905ff55d4d196d5267766217d5705
karnld <karenazul101@gmail.com>
2021-09-22 17:46:55 -0500

    Proyecto en estado inicial funcionando

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', author: 'Karen López', appName: 'WebApp' });
+});
+
+/* Agregando una nueva ruta */
+router.get('/greeting', function(req, res, next){
+  res.status(200).json({message: 'Hola Campeon de la fullstack Web'})
+})
+
+module.exports = router;
