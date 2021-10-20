01350e0cfe70da3dd30784f8e20992f5d2265c9b
karnld <karenazul101@gmail.com>
2021-10-20 14:29:08 -0500

    S28 Materializa Css

--- server/app.js
+++ server/app.js
@@ -8,6 +8,9 @@ import logger from 'morgan';
 import indexRouter from '@s-routes/index';
 import usersRouter from '@s-routes/users';
 
+// Importing configurations
+import configTemplateEngine from '@s-config/template-engine';
+
 //Webpack modules
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
@@ -51,8 +54,7 @@ if (env == 'development') {
 }
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+configTemplateEngine(app);
 
 app.use(logger('dev'));
 app.use(express.json());
